4b55a88b6f0b30bb2a377ac5a47e7b691f5efb8d
umw111 <utheriwagura@gmail.com>
2023-12-09 11:21:02 -0500

    removed grid, changed color of rp1, improved lines animation

--- projective.py
+++ projective.py
@@ -19,11 +19,20 @@ class rp1(Scene):
         self.play(Write(definition))
 
         # Define equivalent points on circle, arrow connecting them
+        lines = []
+        for i in range(1,11):
+            lines.append(Line(start = np.array([2*np.cos(PI/2+(i*PI)/10),2*np.sin(PI/2+(i*PI)/10),0]),
+                            end = np.array([-2*np.cos(PI/2+(i*PI)/10),-2*np.sin(PI/2+(i*PI)/10),0]))) 
+        
         for i in range(10):
-            nuLine = Line(start = np.array([2*np.cos(PI/2+(i*PI)/5),2*np.sin(PI/2+(i*PI)/5),0]),
-                            end = np.array([-2*np.cos(PI/2+(i*PI)/5),-2*np.sin(PI/2+(i*PI)/5),0]))
-            self.play(Create(nuLine))
-            self.remove(nuLine)
+            if i < 2: 
+                self.play(FadeIn(lines[i]),run_time=.1)
+            else: 
+                self.play(FadeIn(lines[i]),FadeOut(lines[i-2]),run_time=.1)
+        self.play(FadeOut(lines[8]))
+        self.play(FadeOut(lines[9]))
+        # equiv_line = Line(start = 2*UP, end = 2*DOWN)
+        # tip = TracedPath(a.get_center, dissipating_time=0.5, stroke_opacity=[0, 1])
 
         self.remove(definition)
         equiv_explain = Tex(
@@ -33,7 +42,9 @@ class rp1(Scene):
         self.play(Write(equiv_explain))
 
         # Combine equivalent points
+        self.play(FadeOut(grid))
         self.play(Transform(bottom, top))
+        self.play(bottom.animate.set_color(RED))
         
         # Mention Homotopy
         self.remove(equiv_explain)
@@ -44,8 +55,8 @@ class rp1(Scene):
         self.play(Write(homot_explain))
         
         # self.remove(bottom)
-        l = Arc(start_angle=PI,angle=PI/2,stroke_width = 8,color = YELLOW)
-        r = Arc(angle=-PI/2,stroke_width = 8,color = YELLOW)
+        l = Arc(start_angle=PI,angle=PI/2,stroke_width = 8,color = RED)
+        r = Arc(angle=-PI/2,stroke_width = 8,color = RED)
         self.play(Create(l),Create(r))
 
         # # Rotate elements together
